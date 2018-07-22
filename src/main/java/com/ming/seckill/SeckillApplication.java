5c3f7aeba78b09e09b9a42e80e13df2dc93a43e5
wolter <mingcanchen@hotmail.com>
2018-07-23 00:14:49 +0800

    first commit

---
+++ src/main/java/com/ming/seckill/SeckillApplication.java
@@ -0,0 +1,15 @@
+package com.ming.seckill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wolter
+ */
+@SpringBootApplication
+public class SeckillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeckillApplication.class, args);
+    }
+}
